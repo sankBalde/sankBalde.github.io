fe2604217649d71207af02cd0493b3993a191191
Abdoulaye Balde <abdoulaye.balde@epita.fr>
2025-06-19 19:01:39 +0200

    changement des noms 002

--- mon-cv-angular/src/app/components/projects/project.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    technologies: string[];
-    githubLink: string;
-    liveDemoLink?: string; // Optionnel
-    imageUrls?: string[]; // Pour les captures d'écran
-  }
